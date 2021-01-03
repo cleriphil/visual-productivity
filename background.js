2dbd1ab8c58887e578c91bba5260382fc42b3b0a
cleriphil <cleri.phil@gmail.com>
2021-01-03 13:26:03 -0800

    save screenshot chrome-extension:// URL with chrome.storage.sync

--- background.js
+++ background.js
@@ -1,23 +1,39 @@
 let id = 100;
 chrome.browserAction.onClicked.addListener(() => {
-chrome.tabs.captureVisibleTab((screenshotUrl) => {
-    const viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
-    let targetId = null;
-chrome.tabs.onUpdated.addListener(function listener(tabId,     changedProps) {
-if (tabId != targetId || changedProps.status != "complete")
-        return;
-chrome.tabs.onUpdated.removeListener(listener);
-const views = chrome.extension.getViews();
-      for (let i = 0; i < views.length; i++) {
-        let view = views[i];
-        if (view.location.href == viewTabUrl) {
-          view.setScreenshotUrl(screenshotUrl);
-          break;
-        }
-      }
+    chrome.tabs.captureVisibleTab((screenshotUrl) => {
+        const viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
+        let targetId = null;
+        chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
+            if (tabId != targetId || changedProps.status != "complete")
+                return;
+            chrome.tabs.onUpdated.removeListener(listener);
+            const views = chrome.extension.getViews();
+            for (let i = 0; i < views.length; i++) {
+                let view = views[i];
+                if (view.location.href == viewTabUrl) {
+                    view.setScreenshotUrl(screenshotUrl);
+                    break;
+                }
+            }
+        });
+        chrome.tabs.create({url: viewTabUrl}, (tab) => {
+        targetId = tab.id;
+        });
+
+        //storage sync code
+        chrome.storage.sync.set({ 'target' : viewTabUrl}, function() {
+            if (chrome.runtime.error) {
+            console.log('Runtime error.');
+            }
+        });
+        chrome.storage.sync.get('target', function(result) {
+            if (!chrome.runtime.error) {
+            console.log('result.target is ' + result.target);
+            }
+        });
     });
-chrome.tabs.create({url: viewTabUrl}, (tab) => {
-      targetId = tab.id;
-    });
-  });
 });
+
+
+
+
--- screenshot.js
+++ screenshot.js
@@ -1,3 +1,4 @@
 function setScreenshotUrl(url) {
     document.getElementById('target').src = url;
   }
+
